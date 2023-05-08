5d97b42e01a787f7229c57e0ee11fb2efbeddc11
Samuel Hyrum <samuelhymp@gmail.com>
2023-05-08 19:50:24 -0300

    Req 6, criando teste de priority queue

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,36 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
+
+archive_1 = {
+    "nome_do_arquivo": "arquivo_teste.txt",
+    "qtd_linhas": 8,
+    "linhas_do_arquivo": [...]
+}
+
+archive_2 = {
+    "nome_do_arquivo": "arquivo_teste.txt",
+    "qtd_linhas": 8,
+    "linhas_do_arquivo": [...]
+}
+
+archive_3 = {
+    "nome_do_arquivo": "arquivo_teste.txt",
+    "qtd_linhas": 2,
+    "linhas_do_arquivo": [...]
+}
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    priority_queue = PriorityQueue()
+
+    priority_queue.enqueue(archive_1)
+    priority_queue.enqueue(archive_2)
+
+    assert priority_queue.dequeue() == archive_2
+
+    priority_queue.enqueue(archive_3)
+
+    assert priority_queue.search(len(priority_queue) - 1) == archive_2
+
+    with pytest.raises(IndexError):
+        priority_queue.search(10)
