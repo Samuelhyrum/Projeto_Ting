bcc5b0422b4d2dfd52763bd6a211023039bb23bf
Samuel Hyrum <samuelhymp@gmail.com>
2023-05-08 17:58:53 -0300

    Erros de linter

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -2,14 +2,12 @@ import sys
 
 
 def txt_importer(file_path):
-    # Verifica a extensão do arquivo
     if not file_path.endswith('.txt'):
         print('Formato inválido', file=sys.stderr)
         return []
-    
+
     try:
         with open(file_path, 'r') as file:
-            # Lê o conteúdo do arquivo e retorna como lista
             return file.read().splitlines()
     except FileNotFoundError:
         print(f'Arquivo {file_path} não encontrado', file=sys.stderr)
